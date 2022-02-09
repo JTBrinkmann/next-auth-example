3bf1428d096f7a563957cccbb6d0b4f19ed9eec2
J.- T. Brinkmann <ppp1pp1p1@googlemail.com>
2022-02-09 14:03:39 +0100

    use getServerSession in /api/*

--- pages/api/examples/protected.ts
+++ pages/api/examples/protected.ts
@@ -1,9 +1,12 @@
 // This is an example of to protect an API route
 import { getSession } from "next-auth/react"
 import type { NextApiRequest, NextApiResponse } from "next"
+import { getServerSession } from "next-auth"
+import { nextAuthConfig } from "../../../next-auth.config"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req })
+  // const session = await getSession({ req })
+  const session = await getServerSession({ req, res }, nextAuthConfig)
 
   if (session) {
     res.send({
--- pages/api/examples/session.ts
+++ pages/api/examples/session.ts
@@ -1,8 +1,11 @@
 // This is an example of how to access a session from an API route
 import { getSession } from "next-auth/react"
 import type { NextApiRequest, NextApiResponse } from "next"
+import { getServerSession } from "next-auth"
+import { nextAuthConfig } from "../../../next-auth.config"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req })
+  // const session = await getSession({ req })
+  const session = await getServerSession({ req, res }, nextAuthConfig)
   res.send(JSON.stringify(session, null, 2))
 }
